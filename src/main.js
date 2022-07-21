ff1f3b9c6d13e2eed65a00f86c71f27dfefa26e1
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-07-21 19:00:59 +0545

    feat: vue fontawesome libraries imported

--- src/main.js
+++ src/main.js
@@ -3,7 +3,19 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "./assets/styles/main.scss";
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+/* import specific icons */
+import { faBorderAll, faDownload } from "@fortawesome/free-solid-svg-icons";
+
+/* /* add each imported icon to the library */
+library.add(faBorderAll, faDownload);
+
 const app = createApp(App);
 app.use(store);
 app.use(router);
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.mount("#app");
