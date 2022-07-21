2d28a4775e9f48edd56bb210201d88acfa65c712
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-07-21 19:00:14 +0545

    refactor: universal styles moved to main.scss file

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,19 @@
 <template>
-  <HomeView />
-  <!-- <AboutView />
-  <ServicesView />
-  <ContactsView /> -->
+  <Navbar />
+  <router-view />
 </template>
 
 <script>
-import HomeView from "@/views/HomeView.vue"
+import Navbar from "@/components/Navbar.vue"
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HomeView,
-    // AboutView,
-    // ServicesView,
-    // ContactsView,
+    Navbar,
   }
 }
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
 
-  *,
-  ::before,
-  ::after {
-    box-sizing: border-box;
-    padding: 0;
-    margin: 0;
-  }
-
-  body {
-    font-family: 'Poppins', sans-serif;
-    font-size: 16px;
-    line-height: 16px;
-    font-weight: 500;
-    color: #3E4581;
-    background-color: #E5E5E5;
-  }
 </style>
