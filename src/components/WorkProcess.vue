b58be603a5e6df0da20d03ac23592ecb796bd778
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-07-22 15:36:43 +0545

    feat: new components and images added along with sass partials

---
+++ src/components/WorkProcess.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="work">
+    <header class="work-header">
+      <h1 class="header">How It Works?</h1>
+      <p class="subheader">
+        Resolving neglected sir tolerably but existence conveying for. Day his
+        put off unaffected literature partiality inhabiting.
+      </p>
+    </header>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@use "../assets/styles/abstracts" as a;
+
+.work {
+  .work-header {
+    .header {
+      @include a.header;
+    }
+
+    .subheader {
+      width: 45%;
+    }
+  }
+}
+</style>
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Hero } from "./Hero.vue";
+export { default as Navbar } from "./Navbar.vue";
+export { default as Clients } from "./Clients.vue";
+export { default as OurServices } from "./OurServices.vue";
+export { default as WorkProcess } from "./WorkProcess.vue";
