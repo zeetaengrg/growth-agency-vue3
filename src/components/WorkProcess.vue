a4a112d3e3bd002859e3594326cbe74f254a50e6
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-08-04 22:04:53 +0545

    style: markups styled accordingly

--- src/components/WorkProcess.vue
+++ src/components/WorkProcess.vue
@@ -68,33 +68,149 @@
 
 .work {
   display: flex;
-  padding: 1rem 8rem;
+  margin-top: 4rem;
+  padding: 0rem 8rem;
   gap: 5rem;
+  position: relative;
 
   @mixin card {
     background-color: a.$primary-clr-4;
-    /* height: 29rem; */
     width: 31rem;
     border-radius: 1rem;
+    padding: 3rem;
+    background-image: url("../assets/images/union.svg");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
   }
 
-  .work-left {
+  .work__left-col {
     flex: 1;
     display: flex;
     flex-direction: column;
     gap: 1rem;
 
-    &__header {
+    .work__header {
       @include a.header;
     }
 
-    &__subheader {
+    .work__subheader {
       @include a.subheader;
     }
 
-    .work-left-card {
+    .work__card {
       @include card;
       margin-top: 1rem;
+      gap: 3rem;
+
+      @mixin card-content {
+        background-color: a.$secondary-clr;
+        border-radius: 1rem;
+        width: 20rem;
+        display: flex;
+      }
+
+      #card-top-content {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        .work__card-content:nth-child(1) {
+          @include card-content;
+          padding: 2rem;
+          height: 10rem;
+          flex-direction: column;
+          gap: 1rem;
+
+          .work__bar-one {
+            background: a.$bg-gradient;
+            height: 0.5rem;
+            width: 75%;
+            border-radius: 1rem;
+          }
+
+          .work__bar-two {
+            background: a.$bg-gradient;
+            height: 0.5rem;
+            width: 95%;
+            border-radius: 1rem;
+          }
+
+          .work__img-wrapper {
+            display: flex;
+            align-items: center;
+            gap: 1rem;
+
+            .work__profile-img {
+              height: 3rem;
+              width: 3rem;
+              border-radius: 50%;
+              background-color: a.$bg-gradient;
+              border: 1px solid a.$bg-gradient;
+            }
+
+            .work__bar-three {
+              background: a.$bg-gradient;
+              height: 0.5rem;
+              width: 30%;
+              border-radius: 1rem;
+            }
+          }
+        }
+
+        .work__card-content:nth-child(2) {
+          @include card-content;
+          align-items: center;
+          gap: 1rem;
+          padding: 1.5rem;
+
+          .work__icon-wrapper {
+            display: grid;
+            place-content: center;
+            background-color: a.$primary-clr-1;
+            height: 2rem;
+            width: 2rem;
+            border-radius: 0.5rem;
+            color: a.$secondary-clr;
+          }
+
+          .work__bar-four {
+            background: a.$bg-gradient;
+            height: 0.5rem;
+            width: 80%;
+            border-radius: 1rem;
+          }
+        }
+      }
+
+      .work__card-info {
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+
+        .work__card-desc {
+          font-size: 0.8rem;
+          font-weight: 400;
+          max-width: 75%;
+          margin: 0 auto;
+        }
+      }
+    }
+  }
+
+  .work__right-col {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 4rem;
+
+    .work__card {
+      @include card;
+      height: 29rem;
 
       &__img {
         width: 100%;
@@ -102,18 +218,21 @@
     }
   }
 
-  .work-right {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    gap: 4rem;
+  .work__aside {
+    position: absolute;
+    left: 0rem;
+    bottom: -4rem;
 
-    .work-right-card {
-      @include card;
+    .work__aside-arrow {
+      width: 12rem;
+      transform: scale(1, -1);
+    }
 
-      &__img {
-        width: 100%;
-      }
+    .work__aside-star {
+      position: absolute;
+      width: 5rem;
+      top: -2.5rem;
+      left: 9.5rem;
     }
   }
 }
