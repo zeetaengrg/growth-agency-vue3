2c459dd1637fee8002021c2b6f0b034cbc9adb04
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-08-05 19:47:35 +0545

    refactor: Navbar component added for reusability on every pages

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
+  <Navbar />
   <router-view />
 </template>
 
 <script>
+import { Navbar } from "@/components/templates";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Navbar,
+  },
 };
 </script>
 
