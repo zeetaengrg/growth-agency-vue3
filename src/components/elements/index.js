af1cba413236b9b950456b06678592c91f444400
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-08-05 19:55:12 +0545

    feat: new components added to export

--- src/components/elements/index.js
+++ src/components/elements/index.js
@@ -1,1 +1,4 @@
+export { default as CardProfile } from "./CardProfile/CardProfile.vue";
 export { default as SectionHeader } from "./SectionHeader/SectionHeader.vue";
+export { default as CardHeader } from "./CardHeader/CardHeader.vue";
+export { default as Ratings } from "./Ratings/Ratings.vue";
